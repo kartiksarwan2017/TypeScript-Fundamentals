6b0d840c329a0dfc35178d7d3f98a2accf9c2a5d
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-11-11 15:21:10 +0530

    Typescript Basics

---
+++ sample.js
@@ -0,0 +1,6 @@
+// const a = "Nice";
+// console.log(a);
+// const b = 232;
+// console.log(b); 
+var a = 4444;
+console.log(a);
---
+++ sample.ts
@@ -0,0 +1,10 @@
+// const a = "Nice";
+// console.log(a);
+
+
+// const b = 232;
+// console.log(b); 
+
+
+const a = 4444;
+console.log(a);
